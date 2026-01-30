4b6382ecaaaefd853ef429da5bf9f7fc1d5cd9c3
Danny Ben Shitrit <db@dannyben.com>
2026-01-30 10:02:43 +0200

    add config clear test

--- test/spec/config.sh
+++ test/spec/config.sh
@@ -11,3 +11,16 @@ describe "config"
 
   approve "nlc config create --help"
   approve "nlc config create"
+
+  approve "nlc config clear --help"
+
+  describe "deleting config"
+    mkdir -p tmp
+    cp fixtures/nlc_config.ini tmp
+    NLC_CONFIG=tmp/nlc_config.ini
+    
+    approve "nlc config clear"
+    [[ -f "$NLC_CONFIG" ]] && fail "$NLC_CONFIG expected to be gone"
+    
+    NLC_CONFIG=fixtures/nlc_config.ini
+
